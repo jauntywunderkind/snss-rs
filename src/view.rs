9af15ef8ab187d0d5040909dfdade24790794118
jauntywunderkind <jaunty+wunder+kind+scm@voodoowarez.com>
2023-05-20 22:42:21 -0400

    trivial binary_layout example

--- src/view.rs
+++ src/view.rs
@@ -1,17 +1,26 @@
 use std::fs::File;
-use std::io::Read;
 
+extern crate binary_layout;
 extern crate memmap2;
+use binary_layout::prelude::*;
 use memmap2::Mmap;
 
+define_layout!(markdown_h1, BigEndian, {
+  marker: u8,
+  space: u8,
+  title: [u8],
+});
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut file = File::open("README.md")?;
-
-    let mut contents = Vec::new();
-    file.read_to_end(&mut contents)?;
-
+    let file = File::open("README.md")?;
     let mmap = unsafe { Mmap::map(&file)? };
 
-    assert_eq!(&contents[..], &mmap[..]);
+    let view = markdown_h1::View::new(mmap);
+
+    let marker = view.marker().read();
+    let title = view.title();
+
+    print!("got {} {}", marker, &title[0]);
+
     Ok(())
 }
