94cbc93ba250d5f51c871c2482ce17cca61843e5
jauntywunderkind <jaunty+wunder+kind+scm@voodoowarez.com>
2023-05-20 23:23:10 -0400

    snss: stub out basic shapes

--- src/snss.rs
+++ src/snss.rs
@@ -5,10 +5,39 @@ extern crate memmap2;
 use binary_layout::prelude::*;
 use memmap2::Mmap;
 
-define_layout!(markdown_h1, BigEndian, {
-  marker: u8,
-  space: u8,
-  title: [u8],
+define_layout!(snss_header, LittleEndian, {
+    header: [u8, 4],
+    version: [u8, 4],
+});
+
+define_layout!(snss_packet, LittleEndian, {
+    length: u16,
+    data: [u8],
+});
+
+define_layout!(snss_navigation_entry, LittleEndian, {
+    session_id: i32,
+    index: i32,
+    // variable length fields, will have to handle...
+    //url: [u8],
+    //title: [u16],
+    page_state_length: i32,
+    //page_state_raw: [u8]
+    transition_type: u32,
+    type_mask: u32,
+    //referrer_url: [u8]
+    original_request_url: u32,
+    is_overriding_user_agent: bool,
+    //timestamp: datetime,
+    //search_terms: [u16],
+    http_status: u32,
+    referrer_policy: i32,
+    extended_map_size: i32,
+    // [string, string] kv
+    task_id: i64,
+    parent_task_id: i64,
+    root_task_id: i64,
+    child_task_id_count: i32,
 });
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
